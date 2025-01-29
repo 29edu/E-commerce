dbf805d1bb85a801430828d621723cdcdb156cb4
Edison Priyadarshi <edisonpriyadarshi12@gmail.com>
2025-01-30 03:36:45 +0530

    Completed UI design of header

--- my-project/src/components/Header/index.jsx
+++ my-project/src/components/Header/index.jsx
@@ -1,44 +1,124 @@
-import {React} from 'react'
+import { React } from "react";
 import "tailwindcss";
-import { Link } from 'react-router-dom';
-import Search from '../Search';
+import { Link } from "react-router-dom";
+import Search from "../Search";
+import Badge from "@mui/material/Badge";
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { IoIosGitCompare } from "react-icons/io";
+import { FaRegHeart } from "react-icons/fa";
+import Tooltip from "@mui/material/Tooltip";
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: "0 4px",
+  },
+}));
 
 const Header = () => {
   return (
     <header>
-      <div className='top-strip py-2 border-t-[1px] border-gray-250 border-b-[1px] border-gray-100'>
-        <div className='container'>
-          <div className='flex items-center justify-between'>
-            <div className='col1 w-[50%]'>
-              <p className='text-[14px] font-[500]'>Get up to 50% off new season styles, limited time only</p>
+      <div className="top-strip py-2 border-t-[1px] border-gray-250 border-b-[1px] border-gray-100">
+        <div className="container">
+          <div className="flex items-center justify-between">
+            <div className="col1 w-[50%]">
+              <p className="text-[14px] font-[500]">
+                Get up to 50% off new season styles, limited time only
+              </p>
             </div>
 
-            <div className='col2 flex items-center justify-end'>
-              <ul className='flex items-center gap-3'>
-                <li className='list-none'>
-                  <Link to="/help-center" className='text-[13px] link font-[500] transition '>Help Center</Link>
+            <div className="col2 flex items-center justify-end">
+              <ul className="flex items-center gap-3">
+                <li className="list-none">
+                  <Link
+                    to="/help-center"
+                    className="text-[13px] link font-[500] transition "
+                  >
+                    Help Center
+                  </Link>
                 </li>
-                <li className='list-none'>
-                  <Link to="/order-tracking" className='text-[13px] link font-[500] transition '>Order Tracking</Link>
+                <li className="list-none">
+                  <Link
+                    to="/order-tracking"
+                    className="text-[13px] link font-[500] transition "
+                  >
+                    Order Tracking
+                  </Link>
                 </li>
               </ul>
             </div>
           </div>
         </div>
       </div>
-      
-      <div className='header py-3'>
-        <div className='container flex items-center justify-between'>
-          <div className='col1 w-[25%]'>
-            <Link to={"/"}><img src='/flipkart.png' className='h-20 w-40'/></Link>
+
+      <div className="header py-3">
+        <div className="container flex items-center justify-between">
+          <div className="col1 w-[25%]">
+            <Link to={"/"}>
+              <img src="/flipkart.png" className="h-20 w-40" />
+            </Link>
           </div>
-          <div className='col2 w-[45%]' >
+
+          <div className="col2 w-[45%]">
             <Search />
           </div>
-          <div className='col3 w-[30%] '></div>
+
+          <div className="col3 w-[30%] flex items-center pl-7">
+            <ul className="flex items-center justify-end gap-3 w-full">
+              <li className="list-none">
+                <Link
+                  to="/login"
+                  className="link transition text-[15px] font-[500] pl-7"
+                >
+                  Log in
+                </Link>{" "}
+                | &nbsp;
+                <Link
+                  to="/register"
+                  className="link transition text-[15px] font-[500]"
+                >
+                  Register
+                </Link>
+              </li>
+
+              <li>
+                <Tooltip title="Cart">
+                  <IconButton aria-label="cart">
+                    <StyledBadge badgeContent={4} color="secondary">
+                      <AiOutlineShoppingCart />
+                    </StyledBadge>
+                  </IconButton>
+                </Tooltip>
+              </li>
+
+              <li>
+                <Tooltip title="Compare">
+                  <IconButton aria-label="cart">
+                    <StyledBadge badgeContent={4} color="secondary">
+                      <IoIosGitCompare />
+                    </StyledBadge>
+                  </IconButton>
+                </Tooltip>
+              </li>
+
+              <li>
+                <Tooltip title="Wishlist">
+                  <IconButton aria-label="cart">
+                    <StyledBadge badgeContent={4} color="secondary">
+                      <FaRegHeart />
+                    </StyledBadge>
+                  </IconButton>
+                </Tooltip>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 export default Header;
--- my-project/src/components/Search/index.jsx
+++ my-project/src/components/Search/index.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-// import "../Search/style.css"
+import "../Search/style.css";
+import Button from '@mui/material/Button';
+import { IoSearch } from "react-icons/io5";
 
 const Search = () => {
     return (
         <div className="searchBox w-[100%] h-[50px] bg-[#e5e5e5] rounded-[5px] relative p-2">
-            <input className=" w-[100%] h-[35px] focus:outline-none p-[2] text-[15px]" type="text" placeholder="Search for Products..."/>
+            <input className=" w-[100%] h-[35px] focus:outline-none p-[2] text-[15px] pl-[10px]" type="text" placeholder="Search for Products..."/>
+            <Button className=" !absolute top-[5px] right-[5px] z-50 !w-[35px] !min-w-[35] h-[40px] !rounded-full text-black">
+                <IoSearch className="!text-black text-[20px]"/>
+            </Button>   {/*Intially the search was realtive but !absolute is used to remove the relative */}
+            
         </div>
+        
     )
 }
 
