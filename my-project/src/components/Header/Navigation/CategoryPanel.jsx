30258f6f4b061773442b18db2bed16a97cf8c5c0
Edison Priyadarshi <edisonpriyadarshi12@gmail.com>
2025-02-12 20:37:21 +0530

    Added Fashion and its components of side war and worked on Fashion in navigation bar to open Some menu

--- my-project/src/components/Header/Navigation/CategoryPanel.jsx
+++ my-project/src/components/Header/Navigation/CategoryPanel.jsx
@@ -2,59 +2,227 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
-import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
 import { useState, useEffect } from "react";
+import { IoClose } from "react-icons/io5";
+import { IoIosArrowForward } from "react-icons/io";
+import { Link } from "react-router-dom";
+import { IoIosArrowDown } from "react-icons/io";
 
 const CategoryPanel = (props) => {
-  
   // To check the state whether the drawer is opne or close
   // useEffect(() => {
   //   alert(props.isOpenCatPanel)
   // },[])
 
-  const [open, setOpen] = useState(false);
+  const [submenuIndex, setSubmenuIndex] = useState(null);
+  const [innerSubmenuIndex, setInnerSubmenuIndex] = useState(null);
 
   const toggleDrawer = (newOpen) => () => {
-  
-    props.setIsOpenCatPanel(newOpen)
+    props.setIsOpenCatPanel(newOpen);
   };
 
+  const openSubmenu = (index) => {
+    if (submenuIndex == index) {
+      setSubmenuIndex(null);
+    } else {
+      setSubmenuIndex(index);
+    }
+  };
+
+  const openInnerSubmenu = (index) => {
+    if (innerSubmenuIndex === index) {
+      setInnerSubmenuIndex(null);
+    } else {
+      setInnerSubmenuIndex(index);
+    }
+  };
 
   const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+    <Box sx={{ width: 250 }} role="presentation" className="categoryPanel">
+      <h3 className="p-3 text-[16px] font-[500] flex items-center justify-between">
+        Shop By categories{" "}
+        <IoClose
+          onClick={toggleDrawer(false)}
+          className="cursor-pointer text-[20px]"
+        />
+      </h3>
 
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+      <div className="scroll">
+        <ul className="w-full">
+          <li className="list-none flex items-center relative flex-col">
+            <Link to="/" className="w-full">
+              <Button className="w-full !text-left !justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
+                Fashion
+              </Button>
+            </Link>
+
+            {submenuIndex === 0 ? (
+              <IoIosArrowDown
+                className="absolute top-[10px] right-[15px] cursor-pointer "
+                onClick={() => openSubmenu(0)}
+              />
+            ) : (
+              <IoIosArrowForward
+                className="absolute top-[10px] right-[15px] cursor-pointer "
+                onClick={() => openSubmenu(0)}
+              />
+            )}
+
+            {submenuIndex === 0 && (
+              <ul className="submenu w-full pl-3">
+                <li className="list-none relative">
+                  <Link to="/" className="w-full">
+                    <Button className="w-full !text-left !justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
+                      Apparel
+                    </Button>
+                  </Link>
+
+                  {innerSubmenuIndex === 0 ? (
+                    <IoIosArrowDown
+                      className="absolute top-[10px] right-[15px] cursor-pointer "
+                      onClick={() => openInnerSubmenu(0)}
+                    />
+                  ) : (
+                    <IoIosArrowForward
+                      className="absolute top-[10px] right-[15px] cursor-pointer "
+                      onClick={() => openInnerSubmenu(0)}
+                    />
+                  )}
+
+                  {innerSubmenuIndex === 0 && (
+                    <ul className="inner_submenu w-full pl-3">
+                      <li className="list-none relative mb-1">
+                        <Link
+                          to="/"
+                          className=" link w-full !text-left !justify-start !px-3 transitio text-[14px]"
+                        >
+                          Smart Tablet
+                        </Link>
+                      </li>
+
+                      <li className="list-none relative mb-1">
+                        <Link
+                          to="/"
+                          className=" link w-full !text-left !justify-start !px-3 transitio text-[14px]"
+                        >
+                          Crepe T-shirt
+                        </Link>
+                      </li>
+
+                      <li className="list-none relative mb-1">
+                        <Link
+                          to="/"
+                          className=" link w-full !text-left !justify-start !px-3 transitio text-[14px]"
+                        >
+                          leather Watch
+                        </Link>
+                      </li>
+
+                      <li className="list-none relative mb-1">
+                        <Link
+                          to="/"
+                          className=" link w-full !text-left !justify-start !px-3 transitio text-[14px]"
+                        >
+                          Rolling Diamond
+                        </Link>
+                      </li>
+                    </ul>
+                  )}
+                </li>
+              </ul>
+            )}
+          </li>
+
+          <li className="list-none flex items-center relative flex-col">
+            <Link to="/" className="w-full">
+              <Button className="w-full !text-left !justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
+                Outwear
+              </Button>
+            </Link>
+
+            {submenuIndex === 1 ? (
+              <IoIosArrowDown
+                className="absolute top-[10px] right-[15px] cursor-pointer "
+                onClick={() => openSubmenu(1)}
+              />
+            ) : (
+              <IoIosArrowForward
+                className="absolute top-[10px] right-[15px] cursor-pointer "
+                onClick={() => openSubmenu(1)}
+              />
+            )}
+
+            {submenuIndex === 1 && (
+              <ul className="submenu w-full pl-3">
+                <li className="list-none relative">
+                  <Link to="/" className="w-full">
+                    <Button className="w-full !text-left !justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
+                      Apparel
+                    </Button>
+                  </Link>
+
+                  {innerSubmenuIndex === 1 ? (
+                    <IoIosArrowDown
+                      className="absolute top-[10px] right-[15px] cursor-pointer "
+                      onClick={() => openInnerSubmenu(1)}
+                    />
+                  ) : (
+                    <IoIosArrowForward
+                      className="absolute top-[10px] right-[15px] cursor-pointer "
+                      onClick={() => openInnerSubmenu(1)}
+                    />
+                  )}
+
+                  {innerSubmenuIndex === 1 && (
+                    <ul className="inner_submenu w-full pl-3">
+                      <li className="list-none relative mb-1">
+                        <Link
+                          to="/"
+                          className=" link w-full !text-left !justify-start !px-3 transitio text-[14px]"
+                        >
+                          Smart Tablet
+                        </Link>
+                      </li>
+
+                      <li className="list-none relative mb-1">
+                        <Link
+                          to="/"
+                          className=" link w-full !text-left !justify-start !px-3 transitio text-[14px]"
+                        >
+                          Crepe T-shirt
+                        </Link>
+                      </li>
+
+                      <li className="list-none relative mb-1">
+                        <Link
+                          to="/"
+                          className=" link w-full !text-left !justify-start !px-3 transitio text-[14px]"
+                        >
+                          leather Watch
+                        </Link>
+                      </li>
+
+                      <li className="list-none relative mb-1">
+                        <Link
+                          to="/"
+                          className=" link w-full !text-left !justify-start !px-3 transitio text-[14px]"
+                        >
+                          Rolling Diamond
+                        </Link>
+                      </li>
+                    </ul>
+                  )}
+                </li>
+              </ul>
+            )}
+          </li>
+        </ul>
+      </div>
     </Box>
   );
 
   return (
     <>
-      {/* <Button onClick={toggleDrawer(true)} > Open Drawer</Button> */}
       <Drawer open={props.isOpenCatPanel} onClose={toggleDrawer(false)}>
         {DrawerList}
       </Drawer>
--- my-project/src/components/Header/Navigation/index.jsx
+++ my-project/src/components/Header/Navigation/index.jsx
@@ -4,84 +4,214 @@ import { IoMenu } from "react-icons/io5";
 import { FaAngleDown } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import { GoRocket } from "react-icons/go";
-import "../Navigation/style.css"
+import "../Navigation/style.css";
 import CategoryPanel from "./CategoryPanel";
+import "../Navigation/style.css";
 
 const Navigation = () => {
+  const [isOpenCatPanel, setIsOpenCatPanel] = useState(false);
 
-    const [isOpenCatPanel, setIsOpenCatPanel] = useState(false)
-
-    const openCategoryPanel = () =>{
-        setIsOpenCatPanel(true);
-    }
+  const openCategoryPanel = () => {
+    setIsOpenCatPanel(true);
+  };
 
   return (
     <>
-        <nav className="py-2">
-      <div className="container flex items-center justify-end gap-9">
-        <di className="col1 w-[15%] ">
-          <Button className="Nav !text-black gap-2 w-full" sx={{ textTransform: "none" }} onClick={openCategoryPanel}>  
-            <IoMenu className="menu-icon size-5" />
-            Categories
-            <FaAngleDown className="text-[14px] ml-auto font-bold" />
-          </Button>
-        </di>
+      <nav className="py-2">
+        <div className="container flex items-center justify-end gap-9">
+          <di className="col1 w-[15%] ">
+            <Button
+              className="Nav !text-black gap-2 w-full"
+              sx={{ textTransform: "none" }}
+              onClick={openCategoryPanel}
+            >
+              <IoMenu className="menu-icon size-5" />
+              Categories
+              <FaAngleDown className="text-[14px] ml-auto font-bold " />
+            </Button>
+          </di>
 
-        <div className="col_2 w-[60%]">
-          <ul className="flex items-center gap-5">
-            <li className="list-none">
-              <Link to="/" className="link transition text-[14px] font-[500]">
-                <Button sx={{ textTransform: "none" }} className="link transition ">Home</Button>
-              </Link>
-            </li>
-            <li className="list-none">
-              <Link to="/" className="link transition text-[14px] font-[500]">
-                <Button sx={{ textTransform: "none" }} className="link transition ">Fashion</Button>
-              </Link>
-            </li>
-            <li className="list-none">
-              <Link to="/" className="link transition text-[14px] font-[500]">
-                <Button sx={{ textTransform: "none" }} className="link transition ">Electronics</Button>
-              </Link>
-            </li>
-            <li className="list-none">
-              <Link to="/" className="link transition text-[14px] font-[500]">
-                <Button sx={{ textTransform: "none" }} className="link transition ">Bags</Button>
-              </Link>
-            </li>
-            <li className="list-none">
-              <Link to="/" className="link transition text-[14px] font-[500]">
-                <Button sx={{ textTransform: "none" }} className="link transition ">Footwear</Button>
-              </Link>
-            </li>
-            <li className="list-none">
-              <Link to="/" className="link transition text-[14px] font-[500]">
-                <Button sx={{ textTransform: "none" }} className="link transition ">Groceries</Button>
-              </Link>
-            </li>
-            <li className="list-none ">
-              <Link to="/" className="link transition text-[14px] font-[500]">
-                <Button sx={{ textTransform: "none" }} className="link transition ">Beauty</Button>
-              </Link>
-            </li>
-            <li className="list-none">
-              <Link to="/" className="link transition text-[14px] font-[500]">
-                <Button sx={{ textTransform: "none" }} className="link transition ">Jewellary</Button>
-              </Link>
-            </li>
-          </ul>
+          <div className="col_2 w-[60%]">
+            <ul className="flex items-center gap-5 nav">
+              <li className="list-none">
+                <Link to="/" className="link transition text-[14px] font-[500]">
+                  <Button
+                    sx={{ textTransform: "none" }}
+                    className="link transition"
+                  >
+                    Home
+                  </Button>
+                </Link>
+              </li>
+              <li className="list-none relative">
+                <Link to="/" className="link transition text-[14px] font-[500]">
+                  <Button
+                    sx={{ textTransform: "none" }}
+                    className="link transition "
+                  >
+                    Fashion
+                  </Button>
+                </Link>
+
+                <div className="submenu absolute top-[120%] left-[0%] min-w-[200px] bg-white shadow-md opacity-0 transition-all">
+                  <ul>
+                    <li className="list-none w-full">
+                      <Link to="/" className="w-full">
+                        <Button className="!capitalize !text-[rgba(0,0,0,0,0.8)] w-full !text-left !justify-start !rounded-none">
+                          Men
+                        </Button>
+
+
+
+                        <div className="submenu absolute top-[0%] left-[100%] min-w-[200px] bg-white shadow-md opacity-0 transition-all">
+                          <ul>
+                            <li className="list-none w-full relative">
+                              <Link to="/" className="w-full">
+                                <Button className="!capitalize !text-[rgba(0,0,0,0,0.8)] w-full !text-left !justify-start !rounded-none">
+                                  T shirt
+                                </Button>
+                              </Link>
+                            </li>
+                            <li className="list-none w-full">
+                              <Link to="/" className="w-full">
+                                <Button className="!capitalize !text-[rgba(0,0,0,0,0.8)] w-full !text-left !justify-start !rounded-none">
+                                  Jeans
+                                </Button>
+                              </Link>
+                            </li>
+                            <li className="list-none w-full">
+                              <Link to="/" className="w-full">
+                                <Button className="!capitalize !text-[rgba(0,0,0,0,0.8)] w-full !text-left !justify-start !rounded-none">
+                                  FootWear
+                                </Button>
+                              </Link>
+                            </li>
+                            <li className="list-none w-full">
+                              <Link to="/" className="w-full">
+                                <Button className="!capitalize !text-[rgba(0,0,0,0,0.8)] w-full !text-left !justify-start !rounded-none">
+                                  Shirt
+                                </Button>
+                              </Link>
+                            </li>
+                            <li className="list-none w-full">
+                              <Link to="/" className="w-full">
+                                <Button className="!capitalize !text-[rgba(0,0,0,0,0.8)] w-full !text-left !justify-start !rounded-none">
+                                  Pant
+                                </Button>
+                              </Link>
+                            </li>
+                          </ul>
+                        </div>
+                      </Link>
+                    </li>
+                    <li className="list-none w-full">
+                      <Link to="/" className="w-full">
+                        <Button className="!capitalize !text-[rgba(0,0,0,0,0.8)] w-full !text-left !justify-start !rounded-none">
+                          Women
+                        </Button>
+                      </Link>
+                    </li>
+                    <li className="list-none w-full">
+                      <Link to="/" className="w-full">
+                        <Button className="!capitalize !text-[rgba(0,0,0,0,0.8)] w-full !text-left !justify-start !rounded-none">
+                          Kids
+                        </Button>
+                      </Link>
+                    </li>
+                    <li className="list-none w-full">
+                      <Link to="/" className="w-full">
+                        <Button className="!capitalize !text-[rgba(0,0,0,0,0.8)] w-full !text-left !justify-start !rounded-none">
+                          Girls
+                        </Button>
+                      </Link>
+                    </li>
+                    <li className="list-none w-full">
+                      <Link to="/" className="w-full">
+                        <Button className="!capitalize !text-[rgba(0,0,0,0,0.8)] w-full !text-left !justify-start !rounded-none">
+                          Boys
+                        </Button>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+
+              <li className="list-none">
+                <Link to="/" className="link transition text-[14px] font-[500]">
+                  <Button
+                    sx={{ textTransform: "none" }}
+                    className="link transition "
+                  >
+                    Electronics
+                  </Button>
+                </Link>
+              </li>
+              <li className="list-none">
+                <Link to="/" className="link transition text-[14px] font-[500]">
+                  <Button
+                    sx={{ textTransform: "none" }}
+                    className="link transition "
+                  >
+                    Bags
+                  </Button>
+                </Link>
+              </li>
+              <li className="list-none">
+                <Link to="/" className="link transition text-[14px] font-[500]">
+                  <Button
+                    sx={{ textTransform: "none" }}
+                    className="link transition "
+                  >
+                    Footwear
+                  </Button>
+                </Link>
+              </li>
+              <li className="list-none">
+                <Link to="/" className="link transition text-[14px] font-[500]">
+                  <Button
+                    sx={{ textTransform: "none" }}
+                    className="link transition "
+                  >
+                    Groceries
+                  </Button>
+                </Link>
+              </li>
+              <li className="list-none ">
+                <Link to="/" className="link transition text-[14px] font-[500]">
+                  <Button
+                    sx={{ textTransform: "none" }}
+                    className="link transition "
+                  >
+                    Beauty
+                  </Button>
+                </Link>
+              </li>
+              <li className="list-none">
+                <Link to="/" className="link transition text-[14px] font-[500]">
+                  <Button
+                    sx={{ textTransform: "none" }}
+                    className="link transition "
+                  >
+                    Jewellary
+                  </Button>
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col3 w-[25%] ">
+            <p className="text-[14px] font-[500] flex justify-end gap-3 mr-10">
+              <GoRocket className="size-5" />
+              Free International Delivery
+            </p>
+          </div>
         </div>
+      </nav>
 
-        <div className="col3 w-[25%] ">
-          <p className="text-[14px] font-[500] flex justify-end gap-3 mr-10">
-            <GoRocket className="size-5" />
-            Free International Delivery
-          </p>
-        </div>
-      </div>
-    </nav>
-
-        <CategoryPanel setIsOpenCatPanel={setIsOpenCatPanel} isOpenCatPanel={isOpenCatPanel}/>
+      <CategoryPanel
+        setIsOpenCatPanel={setIsOpenCatPanel}
+        isOpenCatPanel={isOpenCatPanel}
+      />
     </>
   );
 };
